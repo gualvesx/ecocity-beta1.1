9a3fdee348fba51f30b01cb0c3089d33327c8a01
Gustavo Alves <164105793+gualvesx@users.noreply.github.com>
2025-04-25 09:41:19 -0300

    Update SecaoInterativa.tsx

--- src/components/SecaoInterativa.tsx
+++ src/components/SecaoInterativa.tsx
@@ -15,21 +15,21 @@ const SecaoInterativa = () => {
       id: 'desmatamento',
       titulo:'Impacto Ambiental',
       icon: Trees,
-      cor: 'bg-red-500',
+      cor: 'bg-eco-green',
       descricao: 'Quando o lixo é jogado no lugar errado, o solo perde vida e vira um terreno tóxico!'
     },
     {
       id: 'poluicao',
       titulo: 'Riscos às pessoas',
       icon: BadgeAlert,
-      cor: 'bg-amber-500',
+      cor: 'bg-eco-green',
       descricao: 'Moradores próximos a lixões eletrônicos inalam toxinas e sofrem com doenças graves – descarte incorreto mata!'
     },
     {
       id: 'agua',
       titulo: 'Recursos Hídricos',
       icon: Droplets,
-      cor: 'bg-blue-500',
+      cor: 'bg-eco-green',
       descricao: 'O descarte incorreto de lixo contamina o lençol freático, envenenando a água que bebemos e destruindo fontes naturais de vida!'
     },
     {
