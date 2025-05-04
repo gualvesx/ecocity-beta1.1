a6ba41dbc04bfd9ae8e8be76b3e1ff224ff3902b
Gustavo Alves <164105793+gualvesx@users.noreply.github.com>
2025-05-04 17:23:44 -0300

    api

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'ecocity-beta11-main',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'ecocity-beta11-main',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
