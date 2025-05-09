aba85bd40cf466d9a922140e5f780bd48a9f1df2
Gustavo Alves <164105793+gualvesx@users.noreply.github.com>
2025-05-09 16:52:08 -0300

    Update SecaoInterativa.tsx

--- src/components/SecaoInterativa.tsx
+++ src/components/SecaoInterativa.tsx
@@ -16,7 +16,7 @@ const SecaoInterativa = () => {
       id: 'recycling',
       title: 'Poluição dos Solos',
       description: 'Metais pesados que envenenam a terra por décadas.',
-      icon: '<img src="https://cdn-icons-png.flaticon.com/128/8369/8369148.png" alt"arvore"/>',
+      icon: <Recycle className= "h-10 w-10" />,
       color: 'from-eco-green to-eco-green-light',
       details: 'Resíduos eletrônicos descartados incorretamente liberam chumbo, mercúrio e cádmio que se infiltram no solo. Uma bateria de celular abandonada pode contaminar até 60 mil litros de água com seus componentes tóxicos.'
     },
