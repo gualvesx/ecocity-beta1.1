ae5a306d5e9c14335fc24117cba5e0680b8f13fe
Gustavo Alves <164105793+gualvesx@users.noreply.github.com>
2025-05-09 16:10:54 -0300

    Update SecaoInterativa.tsx

--- src/components/SecaoInterativa.tsx
+++ src/components/SecaoInterativa.tsx
@@ -16,7 +16,7 @@ const SecaoInterativa = () => {
       id: 'recycling',
       title: 'Poluição dos Solos',
       description: 'Metais pesados que envenenam a terra por décadas.',
-      icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M272 96c-78.6 0-145.1 51.5-167.7 122.5c33.6-17 71.5-26.5 111.7-26.5l88 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-16 0-72 0s0 0 0 0c-16.6 0-32.7 1.9-48.3 5.4c-25.9 5.9-49.9 16.4-71.4 30.7c0 0 0 0 0 0C38.3 298.8 0 364.9 0 440l0 16c0 13.3 10.7 24 24 24s24-10.7 24-24l0-16c0-48.7 20.7-92.5 53.8-123.2C121.6 392.3 190.3 448 272 448l1 0c132.1-.7 239-130.9 239-291.4c0-42.6-7.5-83.1-21.1-119.6c-2.6-6.9-12.7-6.6-16.2-.1C455.9 72.1 418.7 96 376 96L272 96z"/></svg>',
+      icon: '',
       color: 'from-eco-green to-eco-green-light',
       details: 'Resíduos eletrônicos descartados incorretamente liberam chumbo, mercúrio e cádmio que se infiltram no solo. Uma bateria de celular abandonada pode contaminar até 60 mil litros de água com seus componentes tóxicos.'
     },
