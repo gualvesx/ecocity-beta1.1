b3bd917411d888c99d63665500b15d35cc824d30
Gustavo Alves <164105793+gualvesx@users.noreply.github.com>
2025-05-09 08:19:30 -0300

    Update ThemeToggle.tsx

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,8 +1,6 @@
-
 import * as React from 'react';
 import { useTheme } from '@/contexts/ThemeContext';
 
-// Changed from React.FC to regular function
 export const ThemeToggle = () => {
   const { theme } = useTheme();
   
